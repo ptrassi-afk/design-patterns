25c74f84d39c0474e536e1ca6e8bd79d41d62131
ptrassi <ptrassi@alunos.utfpr.edu.br>
2026-06-11 23:54:25 -0300

    feat: implement Singleton pattern with Constant class

---
+++ include/creational/singleton/singleton.hpp
@@ -0,0 +1,22 @@
+#ifndef SINGLETON_HPP
+#define SINGLETON_HPP
+
+class Constant
+{
+private:
+    static Constant *instance;
+
+    double pi;
+
+    Constant();
+
+public:
+    Constant(const Constant &) = delete;
+
+    Constant &operator=(const Constant &) = delete;
+    static Constant *getInstance();
+
+    double getPi() const;
+};
+
+#endif
